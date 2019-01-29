783ad2026ce39bc2b846a927b0312e160898562d
Stanley Louis <stanlouis@msn.com>
2019-01-29 15:36:59 -0500

    Add redux store and initial reducer.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './components/App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const DEFAULT_SETTINGS = {
+  gameStarted: false,
+  instructionsExpanded: false,
+};
+const rootReducer = () => {
+  return DEFAULT_SETTINGS;
+};
+
+const store = createStore(rootReducer);
+
+console.log('store', store);
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(<App />, document.getElementById('root'));

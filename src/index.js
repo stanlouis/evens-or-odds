8ac0525008d1ed12a3d9d062389c053c1aa77312
Stanley Louis <stanlouis@msn.com>
2019-01-29 21:14:28 -0500

    Test action creators and reducers.

--- src/index.js
+++ src/index.js
@@ -8,13 +8,54 @@ const DEFAULT_SETTINGS = {
   gameStarted: false,
   instructionsExpanded: false,
 };
-const rootReducer = () => {
-  return DEFAULT_SETTINGS;
+
+const SET_GAME_STARTED = 'SET_GAME_STARTED';
+const SET_INSTRUCTIONS_EXPENDED = 'SET_INSTRUCTIONS_EXPENDED';
+
+const rootReducer = (state = DEFAULT_SETTINGS, action) => {
+  switch (action.type) {
+    case SET_GAME_STARTED:
+      return {
+        gameStarted: action.gameStarted,
+        ...state,
+      };
+    case SET_INSTRUCTIONS_EXPENDED:
+      return {
+        ...state,
+        gameStarted: action.gameStarted,
+      };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(rootReducer);
 
-console.log('store', store);
-console.log('store.getState()', store.getState());
+console.log('Initial store.getState()', store.getState());
+
+store.subscribe(() => console.log('store.getState()', store.getState()));
+
+const startGame = () => {
+  return { type: SET_GAME_STARTED, gameStarted: true };
+};
+
+const cancelGame = () => {
+  return { type: SET_GAME_STARTED, gameStarted: false };
+};
+
+const expandInstructions = () => {
+  return { type: SET_INSTRUCTIONS_EXPENDED, instructionsExpanded: true };
+};
+const cancelInstructions = () => {
+  return { type: SET_INSTRUCTIONS_EXPENDED, instructionsExpanded: false };
+};
+
+store.dispatch(startGame());
+store.dispatch(expandInstructions());
+store.dispatch(cancelGame());
+store.dispatch(cancelInstructions());
+
+// store.dispatch({ type: 'foo' });
+// store.dispatch({ type: 'bar' });
 
 ReactDOM.render(<App />, document.getElementById('root'));

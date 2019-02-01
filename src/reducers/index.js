26807730c99d0150946ea9d7aa672e9c8abaa4fc
Stanley Louis <stanlouis@msn.com>
2019-02-01 09:24:58 -0500

    Correct index reducer import.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import settingsReducer from './settings';
 import deckReducer from './deck';
-import gameStateReducer from './GameState';
+import gameStateReducer from './gameState';
 
 export default combineReducers({
   settings: settingsReducer,
